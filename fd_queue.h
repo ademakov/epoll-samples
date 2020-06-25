3489ac07876ab9391794b19601579cbf753e67b8
Aleksey Demakov <ademakov@gmail.com>
2020-06-25 03:24:05 +0300

    Modify the sample

---
+++ fd_queue.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <atomic>
+#include <cstddef>
+#include <cstdint>
+
+template <std::size_t S>
+struct fd_queue
+{
+	static constexpr std::size_t SIZE = S;
+
+	struct slot
+	{
+		alignas(64) std::atomic<std::uint32_t> lock;
+		int data;
+	};
+
+	alignas(64) std::atomic<std::uint32_t> head;
+	alignas(64) std::atomic<std::uint32_t> tail;
+	alignas(64) std::unique_ptr<slot[]> slots;
+
+	fd_queue() : head(0), tail(0), slots(new slot[SIZE])
+	{
+		for (std::size_t i = 0; i < SIZE; i++)
+			slots[i].lock.store(i, std::memory_order_relaxed);
+	}
+
+	std::uint64_t push(int data)
+	{
+		const std::size_t n = tail.fetch_add(1, std::memory_order_relaxed);
+		slot &s = slots[n & (SIZE - 1)];
+
+		std::uint64_t overflows = 0;
+		while (s.lock.load(std::memory_order_acquire) != n) {
+			++overflows;
+			__builtin_ia32_pause();
+		}
+
+		s.data = data;
+		s.lock.store(n + 1, std::memory_order_release);
+
+		return overflows;
+	}
+
+	int pop()
+	{
+		const std::size_t n = head.fetch_add(1, std::memory_order_relaxed);
+		slot &s = slots[n & (SIZE - 1)];
+
+		while (s.lock.load(std::memory_order_acquire) != (n + 1))
+			__builtin_ia32_pause();
+
+		int data = s.data;
+		s.lock.store(n + SIZE, std::memory_order_release);
+
+		return data;
+	}
+};
--- sample.cc
+++ sample.cc
@@ -14,18 +14,25 @@
 
 #include <sys/epoll.h>
 
-#include <atomic>
 #include <memory>
 #include <queue>
 #include <thread>
 
+#include "fd_queue.h"
+
 #define NEVENTS		(128)
 
-#define NPRODUCERS	(3)
-#define NCONSUMERS	(3)
+#define NPINGERS	(4)
+#define NPONGERS	(3)
+#define NPOLLERS	(1)
 
 #define NPSOCKETS	(100)
-#define NMESSAGES	(10000)
+#define NMESSAGES	(25000)
+
+#define QUEUE_SIZE	(1u << 13)
+
+#define PINGER_TIMEOUT	1
+#define POLLER_TIMEOUT	1
 
 struct connection
 {
@@ -43,60 +50,8 @@ struct connection
 	}
 };
 
-struct queue
-{
-	static constexpr size_t SIZE = 2048;
-
-	struct slot
-	{
-		alignas(64) std::atomic<uint32_t> lock;
-		int data;
-	};
-
-	alignas(64) std::atomic<uint32_t> head;
-	alignas(64) std::atomic<uint32_t> tail;
-	alignas(64) std::unique_ptr<slot[]> slots;
-
-	queue()	: head(0), tail(0), slots(new slot[SIZE])
-	{
-		for (size_t i = 0; i < SIZE; i++)
-			slots[i].lock.store(i, std::memory_order_relaxed);
-	}
-
-	size_t push(int data)
-	{
-		const size_t n = tail.fetch_add(1, std::memory_order_relaxed);
-		slot &s = slots[n & (SIZE - 1)];
-
-		size_t overflows = 0;
-		while (s.lock.load(std::memory_order_acquire) != n) {
-			++overflows;
-			__builtin_ia32_pause();
-		}
-
-		s.data = data;
-		s.lock.store(n + 1, std::memory_order_release);
-
-		return overflows;
-	}
-
-	int pop()
-	{
-		const size_t n = head.fetch_add(1, std::memory_order_relaxed);
-		slot &s = slots[n & (SIZE - 1)];
-
-		while (s.lock.load(std::memory_order_acquire) != (n + 1))
-			__builtin_ia32_pause();
-
-		int data = s.data;
-		s.lock.store(n + SIZE, std::memory_order_release);
-
-		return data;
-	}
-};
-
 void
-consumer(queue &queue)
+pong(fd_queue<QUEUE_SIZE> &queue)
 {
 	for (;;) {
 		int fd = queue.pop();
@@ -125,7 +80,7 @@ consumer(queue &queue)
 }
 
 void
-producer(struct sockaddr_in &addr)
+ping(std::vector<sockaddr_in> &addresses)
 {
 	int efd = epoll_create(EPOLL_CLOEXEC);
 	if (efd < 0)
@@ -139,6 +94,7 @@ producer(struct sockaddr_in &addr)
 
 		conns[i].fd = fd;
 
+		sockaddr_in &addr = addresses[i % addresses.size()];
 		if (connect(fd, (struct sockaddr *) &addr, sizeof addr) < 0)
 			error(1, errno, "listen() failed");
 
@@ -164,7 +120,7 @@ producer(struct sockaddr_in &addr)
 		}
 
 		struct epoll_event events[NEVENTS];
-		int n = epoll_wait(efd, events, NEVENTS, 0);
+		int n = epoll_wait(efd, events, NEVENTS, PINGER_TIMEOUT);
 		if (n < 0)
 			error(1, errno, "epoll_wait() failed");
 
@@ -187,105 +143,146 @@ producer(struct sockaddr_in &addr)
 	}
 }
 
+struct poll
+{
+	int efd = -1;
+	int sfd = -1;
+	::sockaddr_in address;
+
+	fd_queue<QUEUE_SIZE> queue;
+	std::uint64_t overflows = 0;
+	std::uint64_t stats[NEVENTS + 1];
+
+	poll()
+	{
+		sfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sfd < 0)
+			error(1, errno, "socket() failed");
+
+		memset(&address, 0, sizeof(address));
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+		address.sin_port = htons(0);
+
+		if (bind(sfd, (struct sockaddr *) &address, sizeof address) < 0)
+			error(1, errno, "bind() failed");
+		if (listen(sfd, 128) < 0)
+			error(1, errno, "listen() failed");
+
+		{
+			socklen_t addrsz = sizeof address;
+			if (getsockname(sfd, (struct sockaddr *) &address, &addrsz))
+				error(1, errno, "getsockname() failed");
+		}
+		{
+			int flags = fcntl(sfd, F_GETFL, 0);
+			if (flags < 0)
+				error(1, errno, "fcntl(..., F_GETFL, ...) failed");
+			if (fcntl(sfd, F_SETFL, flags | O_NONBLOCK) < 0)
+				error(1, errno, "fcntl(..., F_SETFL, ...) failed");
+		}
+
+		efd = epoll_create(EPOLL_CLOEXEC);
+		if (efd < 0)
+			error(1, errno, "epoll_create() failed");
+
+		struct epoll_event ee;
+		ee.events = EPOLLIN;
+		ee.data.fd = sfd;
+		if (epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &ee) < 0)
+			error(1, errno, "epoll_ctl() failed");
+
+		memset(stats, 0, (NEVENTS + 1) * sizeof stats[0]);
+	}
+
+	~poll()
+	{
+		if (efd >= 0)
+			close(efd);
+		if (sfd >= 0)
+			close(sfd);
+	}
+
+	void run(int nsockets)
+	{
+		int closed = 0;
+		struct epoll_event ee;
+		struct epoll_event events[NEVENTS];
+		do {
+			int n = epoll_wait(efd, events, NEVENTS, POLLER_TIMEOUT);
+			if (n < 0)
+				error(1, errno, "epoll_wait() failed");
+			stats[n]++;
+
+			for (int i = 0; i < n; i++) {
+				struct epoll_event &e = events[i];
+				if (e.data.fd == sfd) {
+					for (;;) {
+						int fd = accept(sfd, NULL, NULL);
+						if (fd < 0) {
+							if (errno == EAGAIN || errno == EWOULDBLOCK)
+								break;
+							error(1, errno, "accept() failed");
+						}
+
+						ee.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+						ee.data.fd = fd;
+						if (epoll_ctl(efd, EPOLL_CTL_ADD, fd, &ee) < 0)
+							error(1, errno, "epoll_ctl() failed");
+					}
+				} else if ((e.events & (EPOLLHUP | EPOLLRDHUP)) != 0) {
+					if (epoll_ctl(efd, EPOLL_CTL_DEL, e.data.fd, &ee) < 0)
+						error(1, errno, "epoll_ctl() failed");
+					queue.push(-e.data.fd);
+					++closed;
+				} else {
+					overflows += queue.push(e.data.fd);
+				}
+			}
+		} while (closed < nsockets);
+	}
+};
+
 int
 main(int argc, char **argv)
 {
 	signal(SIGPIPE, SIG_IGN);
 
-	int sfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sfd < 0)
-		error(1, errno, "socket() failed");
-
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-	addr.sin_port = htons(0);
-
-	if (bind(sfd, (struct sockaddr *) &addr, sizeof addr) < 0)
-		error(1, errno, "bind() failed");
-	if (listen(sfd, 128) < 0)
-		error(1, errno, "listen() failed");
-
-	{
-		socklen_t addrsz = sizeof addr;
-		if (getsockname(sfd, (struct sockaddr *) &addr, &addrsz))
-			error(1, errno, "getsockname() failed");
-	}
-	{
-		int flags = fcntl(sfd, F_GETFL, 0);
-		if (flags < 0)
-			error(1, errno, "fcntl(..., F_GETFL, ...) failed");
-		if (fcntl(sfd, F_SETFL, flags | O_NONBLOCK) < 0)
-			error(1, errno, "fcntl(..., F_SETFL, ...) failed");
+	std::thread pollers[NPOLLERS];
+	std::vector<poll> polls(NPOLLERS);
+	for (int i = 0; i < NPOLLERS; i++) {
+		int nsockets = (NPSOCKETS / NPOLLERS) * NPINGERS;
+		if (i < (NPSOCKETS % NPOLLERS))
+			nsockets += NPINGERS;
+		pollers[i] = std::thread(&poll::run, &polls[i], nsockets);
 	}
 
-	int efd = epoll_create(EPOLL_CLOEXEC);
-	if (efd < 0)
-		error(1, errno, "epoll_create() failed");
+	std::thread pongers[NPONGERS];
+	for (int i = 0; i < NPONGERS; i++) {
+		static_assert(NPONGERS >= NPOLLERS);
+		pongers[i] = std::thread(pong, std::ref(polls[i % NPOLLERS].queue));
+	}
 
-	struct epoll_event ee;
-	ee.events = EPOLLIN;
-	ee.data.fd = sfd;
-	if (epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &ee) < 0)
-		error(1, errno, "epoll_ctl() failed");
+	std::vector<::sockaddr_in> addresses(NPOLLERS);
+	for (int i = 0; i < NPOLLERS; i++)
+		addresses[i] = polls[i].address;
+	for (int i = 0; i < NPINGERS; i++)
+		std::thread(ping, std::ref(addresses)).detach();
 
-	for (int p = 0; p < NPRODUCERS; p++)
-		std::thread(producer, std::ref(addr)).detach();
+	for (int i = 0; i < NPOLLERS; i++)
+		pollers[i].join();
+	for (int i = 0; i < NPONGERS; i++)
+		polls[i % NPOLLERS].queue.push(0);
+	for (int i = 0; i < NPONGERS; i++)
+		pongers[i].join();
 
-	queue queue;
-	std::thread consumers[NCONSUMERS];
-	for (int c = 0; c < NCONSUMERS; c++)
-		consumers[c] = std::thread(consumer, std::ref(queue));
-
-	int closed = 0;
-	size_t overflows = 0;
-	int stats[NEVENTS + 1];
-	memset(stats, 0, (NEVENTS + 1) * sizeof stats[0]);
-	struct epoll_event events[NEVENTS];
-	do {
-		int n = epoll_wait(efd, events, NEVENTS, 0);
-		if (n < 0)
-			error(1, errno, "epoll_wait() failed");
-		stats[n]++;
-
-		for (int i = 0; i < n; i++) {
-			struct epoll_event &e = events[i];
-			if (e.data.fd == sfd) {
-				for (;;) {
-					int fd = accept(sfd, NULL, NULL);
-					if (fd < 0) {
-						if (errno == EAGAIN || errno == EWOULDBLOCK)
-							break;
-						error(1, errno, "accept() failed");
-					}
-
-					ee.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
-					ee.data.fd = fd;
-					if (epoll_ctl(efd, EPOLL_CTL_ADD, fd, &ee) < 0)
-						error(1, errno, "epoll_ctl() failed");
-				}
-			} else if ((e.events & (EPOLLHUP | EPOLLRDHUP)) != 0) {
-				if (epoll_ctl(efd, EPOLL_CTL_DEL, e.data.fd, &ee) < 0)
-					error(1, errno, "epoll_ctl() failed");
-				queue.push(-e.data.fd);
-				++closed;
-			} else {
-				overflows += queue.push(e.data.fd);
-			}
-		}
-	} while (closed < NPRODUCERS);
-
-	printf("queue overflows: %zu\n", overflows);
-	printf("epoll stats: [");
-	for (int i = 0; i <= NEVENTS; i++)
-		printf(" %d=%d,", i, stats[i]);
-	printf(" ]\n");
-
-	for (int c = 0; c < NCONSUMERS; c++)
-		queue.push(0);
-	for (int c = 0; c < NCONSUMERS; c++)
-		consumers[c].join();
+	for (int i = 0; i < NPOLLERS; i++) {
+		printf("queue overflows: %lu\n", polls[i].overflows);
+		printf("epoll stats: [");
+		for (int j = 0; j <= NEVENTS; j++)
+			printf(" %d=%lu,", j, polls[i].stats[j]);
+		printf(" ]\n\n");
+	}
 
 	return 0;
 }
